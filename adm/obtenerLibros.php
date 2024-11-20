3cb9e247757dfcfaea2674263d77240dd77c77cd
levijcdfEVG <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-11-20 10:45:41 +0000

    Se ha comentado mejor el uso de is_array

--- adm/obtenerLibros.php
+++ adm/obtenerLibros.php
@@ -12,6 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['curso'])) {
     // Se usa el método de la clase para obtener los libros relacionados al curso
     $libros = $validaciones->obtenerLibrosPorCurso($curso);
 
+    //Verificamos si el resultado es un array
     if (is_array($libros)) {
         echo json_encode($libros);  // Se envían los libros como respuesta en formato JSON
     } else {
